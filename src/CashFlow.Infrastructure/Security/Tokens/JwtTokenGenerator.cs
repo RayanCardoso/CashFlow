5c1c44b162cb591117231a54d6bf1b5889a72f30
RayanCardoso <rayancardoso40@gmail.com>
2026-06-10 22:17:58 -0300

    feat: first implementation JWT token

---
+++ src/CashFlow.Infrastructure/Security/Tokens/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Tokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace CashFlow.Infrastructure.Security.Tokens;
+
+public class JwtTokenGenerator: IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenGenerator(uint expirationTimeMinutes, string signingKey)
+    {
+        _expirationTimeMinutes = expirationTimeMinutes;
+        _signingKey = signingKey;
+    }
+
+    public string Generate(User user)
+    {
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SecurityKey()
+    {
+        var key = Encoding.UTF8.GetBytes(_signingKey);
+
+        return new SymmetricSecurityKey(key);
+    }
+}
